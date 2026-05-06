325ed067e6db33de91c9b63351ecab6c5ee4d82e
hisyamzakariaa <hisyamzakariaa@latconnect60.com>
2026-05-06 18:35:06 +0800

    fix content

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import CustomButton from "@/components/ui/CustomButton";
 import HStack from "@/components/ui/HStack";
 import StarIcon from "@/components/ui/StarIcon";
 import VStack from "@/components/ui/VStack";
@@ -6,27 +7,33 @@ import VStack from "@/components/ui/VStack";
 export default function Home() {
   return (
     <>
-      <VStack className="bg-[#faf9f8] h-screen items-center justify-center">
-        <HStack className="bg-[#F7D4E84D] py-2 px-4 rounded-2xl items-center gap-2">
-          <StarIcon />
-          <p className="text-[#2D3748CC]">Welcome to my portfolio</p>
-        </HStack>
+      <VStack className="bg-[#faf9f8] h-screen items-center justify-center gap-[48px]">
+        <VStack className="gap-[16px] items-center ">
+          <HStack className="bg-[#F7D4E84D] py-2 px-4 rounded-2xl items-center gap-2">
+            <StarIcon />
+            <p className="text-[#2D3748CC]">Welcome to my portfolio</p>
+          </HStack>
 
-        <h1 className="text-[72px]">
-          Hi, I'm <span className="text-[#8B7FA8]">Hisyam Zakaria</span>
-        </h1>
+          <VStack className="gap-[23px]">
+            <h1 className="text-[72px] leading-[72px]">
+              Hi, I'm <span className="text-[#8B7FA8]">Hisyam Zakaria</span>
+            </h1>
 
-        <p className="text-[24px] text-[#6B7280] max-w-[672px] text-center">
-          A creative developer crafting beautiful digital experiences with clean
-          code and thoughtful design
-        </p>
+            <p className="text-[24px] text-[#6B7280] max-w-[672px] text-center leading-[32px]">
+              A creative developer crafting beautiful digital experiences with
+              clean code and thoughtful design
+            </p>
+          </VStack>
+        </VStack>
 
-        <HStack>
-          <Button>View My Work</Button>
+        <HStack className="gap-[16px]">
+          <CustomButton>View My Work</CustomButton>
+
+          <CustomButton className="text-[#2D3748] bg-[#C4DFF680]">
+            Get In Touch
+          </CustomButton>
         </HStack>
       </VStack>
-
-      <VStack>oi</VStack>
     </>
   );
 }
--- components/ui/CustomButton.tsx
+++ components/ui/CustomButton.tsx
@@ -1,11 +1,25 @@
-import React, { HTMLAttributes, ReactNode } from "react";
+import { HTMLAttributes, ReactNode } from "react";
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 
 interface CustomButtonProps extends HTMLAttributes<HTMLButtonElement> {
-  chidlren: ReactNode;
+  children: ReactNode;
 }
 
-function CustomButton({ children }: CustomButtonProps) {
-  return <div>{children}</div>;
+function CustomButton({ children, ...props }: CustomButtonProps) {
+  return (
+    <button
+      {...props}
+      style={{ ...props.style }}
+      className={twMerge(
+        clsx(
+          `bg-[#8B7FA8] py-3 px-6 rounded-[33px] text-white ${props.className}`,
+        ),
+      )}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default CustomButton;
